8f7e04fd53ecae237feadeac9c7bc137118f8730
Naf-Mughal <nafeelaaqib@gmail.com>
2024-11-04 08:55:10 +0500

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <body className={workSans.variable}>
         {children}
         <Toaster />
